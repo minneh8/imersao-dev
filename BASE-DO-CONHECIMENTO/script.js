cc372cd41114263319b61cf3642a89fe1e9fa733
Lucas Minneh <lucasminneh270508@gmail.com>
2025-11-24 00:46:54 -0300

    aula 3 imersão dev

--- BASE-DO-CONHECIMENTO/script.js
+++ BASE-DO-CONHECIMENTO/script.js
@@ -1,7 +1,45 @@
+let cardContainer = document.querySelector(".card-container");
+let campoBusca = document.querySelector("header input");
 let dados = [];
 
 async function iniciarBusca() {
-    let resposta = await fetch("data.json");
-    dados = await resposta.json();
-    console.log(dados);
+    if (dados.length === 0) {
+        try {
+            let resposta = await fetch("data.json");
+            dados = await resposta.json();
+        } catch (erro) {
+            console.error("Erro ao buscar os dados:", erro);
+            return;
+        }
 }
+
+const termoBusca = campoBusca.value.toLowerCase();
+const dadosFiltrados = dados.filter(dado => {
+    return dado.nome.toLowerCase().includes(termoBusca) || 
+           dado.descricao.toLowerCase().includes(termoBusca);
+});
+
+    renderizarCards(dadosFiltrados);
+}
+
+function renderizarCards(dados) {
+    cardContainer.innerHTML = "";
+    for (let dado of dados) {
+        let article = document.createElement("article");
+        article.classList.add("card");
+        article.innerHTML = `
+            <h2>${dado.nome}</h2>
+                <p>
+                    ${dado.ano}
+                </p>
+                <p>
+                    ${dado.descricao}
+                </p>
+                <a href="${dado.link}" target="_blank">Saiba mais</a>
+        `
+        cardContainer.appendChild(article);
+    }
+}
+
+// Adiciona um evento que chama a função de busca sempre que o usuário digita no campo de busca.
+campoBusca.addEventListener("keyup", iniciarBusca);
